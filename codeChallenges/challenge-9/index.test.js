933e6bd47568b57f2f23e01848e964e5c6d339c1
Raghad Abdulhadi <Raghad.Abdulhadi@optimumpartners.co>
2022-09-01 11:37:19 +0300

    feat: find next biggest number from a digit

---
+++ codeChallenges/challenge-9/index.test.js
@@ -0,0 +1,27 @@
+/*
+You have to create a function
+that takes a positive integer number and returns the
+next bigger number formed by the same digits:
+Exanmples:
+nextBigger(12)==21
+nextBigger(513)==531
+nextBigger(2017)==2071
+If no bigger number can be composed using those digits, return -1:
+nextBigger(9)==-1
+nextBigger(111)==-1
+nextBigger(531)==-1
+*/
+const nextBigger = function(num) {
+    const toArray = Array.from(String(num), Number);
+    [toArray[toArray.length - 2], toArray[toArray.length - 1]] = [toArray[toArray.length - 1], toArray[toArray.length - 2]];
+    const swapedNum = +toArray.join("");
+
+    if(swapedNum < num || swapedNum == num || toArray.length == 1) {
+        return -1;
+    }
+    else {
+        return swapedNum;
+    }
+};
+
+// Complexity 0(n)
